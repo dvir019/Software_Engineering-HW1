1c832846105ef0977660d53ea3904edbca1eb11c
Dvir <dvir019@gmail.com>
2020-05-02 16:55:36 +0300

    Add to Library class

--- src/part2/Library.java
+++ src/part2/Library.java
@@ -1,7 +1,73 @@
 package part2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Library {
-    // Add the missing implementation to this class
+
+    private String address;
+    private ArrayList<Book> books;
+
+    public Library(String address) {
+        this.address = address;
+        books = new ArrayList<Book>();
+    }
+
+    public static void printOpeningHours() {
+        System.out.println("Libraries are open daily from 9:00 to 17:00.");
+    }
+
+    public void addBook(Book bookToAdd) {
+        books.add(bookToAdd);
+    }
+
+    public void printAddress() {
+        System.out.println(address);
+    }
+
+    public void borrowBook(String bookTitle) {
+        boolean isBookInCatalog=false;
+
+        for (Book book : books) {
+            if (book.getTitle().equals(bookTitle)) {
+                isBookInCatalog=true;
+                if (book.isBorrowed()) {
+                    System.out.println("Sorry, this book is already borrowed.");
+                } else {
+                    book.borrowed();
+                    System.out.println("You successfully borrowed " + bookTitle);
+                }
+            }
+        }
+        if (!isBookInCatalog){
+            System.out.println("Sorry, this book is not in our catalog.");
+        }
+    }
+
+    public void returnBook(String bookTitle) {
+        for (Book book : books) {
+            if (book.getTitle().equals(bookTitle)) {
+                book.returned();
+                System.out.println("You successfully returned " + bookTitle);
+            }
+        }
+    }
+
+    public void printAvailableBooks(){
+        boolean noAvailableBooks = true;
+
+        for (Book book : books){
+            if (!book.isBorrowed()){
+                noAvailableBooks=false;
+                System.out.println(book.getTitle());
+            }
+        }
+
+        if(noAvailableBooks){
+            System.out.println("No books in catalog.");
+        }
+    }
+
 
     public static void main(String[] args) {
         //Create two libraries
@@ -9,10 +75,10 @@ public class Library {
         Library secondLibrary = new Library("IE&M Faculty");
 
         //Add four books to the first library
-        firstLibrary.addBook (new Book("Harry Potter"));
-        firstLibrary.addBook (new Book("To Kill a Mockingbird"));
-        firstLibrary.addBook (new Book("JAVA"));
-        firstLibrary.addBook (new Book("Calculus For Dummies"));
+        firstLibrary.addBook(new Book("Harry Potter"));
+        firstLibrary.addBook(new Book("To Kill a Mockingbird"));
+        firstLibrary.addBook(new Book("JAVA"));
+        firstLibrary.addBook(new Book("Calculus For Dummies"));
 
         //Print opening hours and the addresses
         System.out.println("Library hours:");
